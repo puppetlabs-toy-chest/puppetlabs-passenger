e8e966966aae0987bb812aea856ba64b672275ae
Gary Larizza <gary@puppetlabs.com>
2011-07-15 10:18:58 -0700

    Apache Configuration
    This commit uses a case statement to setup Passenger
    apache configuration for RHEL and Debian-based systems.

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,6 +21,51 @@ class passenger {
   require gcc
   require apache::dev
   $version=$passenger::params::version
+  case $operatingsystem {
+    'ubuntu', 'debian': {
+      file { '/etc/apache2/mods-available/passenger.load':
+        ensure  => present,
+        content => template('passenger/passenger-load.erb'),
+        owner   => '0',
+        group   => '0',
+        mode    => '0644',
+      }
+
+      file { '/etc/apache2/mods-available/passenger.conf':
+        ensure  => present,
+        content => template('passenger/passenger-enabled.erb'),
+        owner   => '0',
+        group   => '0',
+        mode    => '0644',
+      }
+
+      file { '/etc/apache2/mods-enabled/passenger.load':
+        ensure  => 'link',
+        owner   => '0',
+        group   => '0',
+        mode    => '0777',
+        require => File['/etc/apache2/mods-available/passenger.load'],
+      }
+
+      file { '/etc/apache2/mods-enabled/passenger.conf':
+        ensure  => 'link',
+        owner   => '0',
+        group   => '0',
+        mode    => '0777',
+        require => File['/etc/apache2/mods-available/passenger.conf'],
+      }
+    }
+    'centos', 'fedora', 'redhat': {
+      file { '/etc/httpd/conf.d/passenger.conf':
+        ensure  => present,
+        content => template('passenger/passenger-conf.erb'),
+        owner   => '0',
+        group   => '0',
+        mode    => '0644',
+      }
+    }
+    'darwin':{}
+  }
 
   package {'passenger':
     name   => 'passenger',
