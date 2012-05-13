25188aa1650d170c32c87feb2a13014b6d301330
Gary Larizza <gary@puppetlabs.com>
2012-05-12 20:49:23 -0700

    Use $osfamily for parameters
    Previously the $operatingsystem fact was used to determine parameter
    defaults in the params.pp class. This commit changes that to use the
    $osfamily fact.

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,14 +14,14 @@ class passenger::params {
   $passenger_version  = '3.0.9'
   $passenger_provider = 'gem'
 
-  case $operatingsystem {
-    'debian', 'ubuntu': {
+  case $osfamily {
+    'debian': {
       $passenger_package      = 'passenger'
       $gem_path               = '/var/lib/gems/1.8/gems'
       $gem_binary_path        = '/var/lib/gems/1.8/bin'
       $mod_passenger_location = "/var/lib/gems/1.8/gems/passenger-$passenger_version/ext/apache2/mod_passenger.so"
     }
-    'centos', 'fedora', 'redhat': {
+    'redhat': {
       $passenger_package      = 'passenger'
       $gem_path               = '/usr/lib/ruby/gems/1.8/gems'
       $gem_binary_path        = '/usr/lib/ruby/gems/1.8/gems/bin'
@@ -32,6 +32,9 @@ class passenger::params {
       $gem_path               = '/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin'
       $gem_binary_path        = '/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin'
       $mod_passenger_location = "/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/passenger-$passenger_version/ext/apache2/mod_passenger.so"
+    }
+    default: {
+      fail("Operating system ${::operatingsystem} is not supported with the Passenger module")
     }
   }
 }
